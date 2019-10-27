5da654468dac63e358a043878414291b448f4393
Neil Champakara <nchamp3@uic.edu>
2019-10-27 12:49:36 -0500

    Created service worker

---
+++ sw.js
@@ -0,0 +1,49 @@
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('/sw.js').then(function(registration) {
+      // Registration was successful
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function(err) {
+      // registration failed :(
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  });
+}
+
+
+var CACHE_NAME = 'my-site-cache';
+var urlsToCache = [
+  '/',
+  '/main.css',
+  '/about.html',
+  '/buildQuery.js',
+  '/sw.js',
+  '/pct1.png',
+  '/pct2.png',
+  '/pct3.png'
+];
+
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
